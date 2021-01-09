0aac27f8e4327de9f5651b22227b5a28a0ef9703
jipeng <jipeng@able-elec.com>
2021-01-09 17:32:53 +0800

    æ消息存储类的属性查看

--- store/src/main/java/org/apache/rocketmq/store/DefaultMessageStore.java
+++ store/src/main/java/org/apache/rocketmq/store/DefaultMessageStore.java
@@ -63,33 +63,61 @@ import org.apache.rocketmq.store.index.QueryOffsetResult;
 import org.apache.rocketmq.store.schedule.ScheduleMessageService;
 import org.apache.rocketmq.store.stats.BrokerStatsManager;
 
+//消息存储
 public class DefaultMessageStore implements MessageStore {
     private static final InternalLogger log = InternalLoggerFactory.getLogger(LoggerName.STORE_LOGGER_NAME);
-
+    /**
+     * 消息存储配置
+     */
     private final MessageStoreConfig messageStoreConfig;
     // CommitLog
+    /**
+     * commitLog文件存储实现类
+     */
     private final CommitLog commitLog;
-
+    /**
+     * topic-->ConsumerQueue缓存表
+     */
     private final ConcurrentMap<String/* topic */, ConcurrentMap<Integer/* queueId */, ConsumeQueue>> consumeQueueTable;
-
+    /**
+     * 消息队列文件刷盘服务
+     */
     private final FlushConsumeQueueService flushConsumeQueueService;
-
+    /**
+     * 清除提交日志的服务
+     */
     private final CleanCommitLogService cleanCommitLogService;
-
+    /**
+     * 清除consumerQueue服务
+     */
     private final CleanConsumeQueueService cleanConsumeQueueService;
-
+    /**
+     * 维护索引文件的service
+     */
     private final IndexService indexService;
-
+    /**
+     * 创建MappedFile文件的服务
+     */
     private final AllocateMappedFileService allocateMappedFileService;
-
+    /**
+     * commitLog消息分发服务 根据commitLog文件构建ConsumerQueue IndexFile文件
+     */
     private final ReputMessageService reputMessageService;
-
+    /**
+     * 高可用的服务类
+     */
     private final HAService haService;
-
+    /**
+     * 定时消息服务
+     */
     private final ScheduleMessageService scheduleMessageService;
-
+    /**
+     * 消息存储服务
+     */
     private final StoreStatsService storeStatsService;
-
+    /**
+     * 消息堆外内存缓存
+     */
     private final TransientStorePool transientStorePool;
 
     private final RunningFlags runningFlags = new RunningFlags();
@@ -97,16 +125,29 @@ public class DefaultMessageStore implements MessageStore {
 
     private final ScheduledExecutorService scheduledExecutorService =
         Executors.newSingleThreadScheduledExecutor(new ThreadFactoryImpl("StoreScheduledThread"));
+    /**
+     * broker状态管理器
+     */
     private final BrokerStatsManager brokerStatsManager;
+    /**
+     * 消息拉取长轮询模式消息达到监听器
+     */
     private final MessageArrivingListener messageArrivingListener;
+    /**
+     * broker配置类
+     */
     private final BrokerConfig brokerConfig;
 
     private volatile boolean shutdown = true;
-
+    /**
+     * 文件刷盘监测点
+     */
     private StoreCheckpoint storeCheckpoint;
 
     private AtomicLong printTimes = new AtomicLong(0);
-
+    /**
+     * commitLog转发请求
+     */
     private final LinkedList<CommitLogDispatcher> dispatcherList;
 
     private RandomAccessFile lockFile;
